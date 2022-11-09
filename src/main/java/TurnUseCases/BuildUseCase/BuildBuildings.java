da5287b7daf121b38adcff09014a91524e781db9
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-11-09 14:58:27 -0500

    Added the MortgageProperty class.

--- src/main/java/TurnUseCases/BuildUseCase/BuildBuildings.java
+++ src/main/java/TurnUseCases/BuildUseCase/BuildBuildings.java
@@ -2,11 +2,8 @@ package TurnUseCases.BuildUseCase;
 
 import GameEntities.Player;
 import GameEntities.Tiles.ColorPropertyTile;
-import GameEntities.Tiles.Property;
+import java.lang.Math;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * All methods related to build buildings.
@@ -24,18 +21,37 @@ public class BuildBuildings {
         if(!player.ownsProperty(property)){
             return false;
         }
+        if(property.isMortgaged()){
+            return false;
+        }
         //When player owns all the properties in a Monopoly color group, player can buy houses.
-        String color = property.getColor();
+        String color = property.getColor(); // need to add a method getColor to get the color of the ColorPropertyTile.
         int counter = 0;
         ArrayList<Propertie> properties = player.getProperties();
         for (int i = 0; i < player.getProperties().size(); i++){
-            properties.get(i).getColor().equals(color);
-            counter += 1;
+            if(properties.get(i).getColor().equals(color)) {
+                counter += 1;
+            }
         }
         if (color.equals("Brown") || color.equals("Dark Blue")){
             return counter == 2;
         } else{
             return counter == 3;
+        }
+        //Player has to build equally – this means player can’t build a second house on a property unless player has a house on all the other properties.
+        ArrayList<Propertie> SameColorProperties = new ArrayList<Propertie>();;
+        for (int i = 0; i < player.getProperties().size(); i++){
+            if(properties.get(i).getColor().equals(color)){
+                SameColorProperties.add(properties.get(i));
+            }
+        }
+        for (Propertie sameColorProperty : SameColorProperties) {
+            int a = sameColorProperty.getHouse() + sameColorProperty.getHotel();
+            // need to add two methods getHouse and getHotel in ColorPropertyTile class to get the number of houses and hotels.
+            int b = property.getHouse() + property.getHotel();
+            if (Math.abs(a - b) > 1) {
+                return false;
+            }
         }
         return true;
     }
@@ -48,7 +64,8 @@ public class BuildBuildings {
      */
     public boolean buildHouse (Player player, ColorPropertyTile property){
         if (isBuildable(player, property)){
-            property.houses += 1;
+            property.addHouse();
+            //need to add a method addHouse in ColorPropertyTile class to add the number of house.
             String color = property.getColor();
             switch (color) {
                 case "Brown":
@@ -81,7 +98,8 @@ public class BuildBuildings {
      */
     public boolean buildHotel (Player player, ColorPropertyTile property){
         if (isBuildable(player, property) && property.getHouses() >= 4){
-            property.hotels += 1;
+            property.addHotel();
+            //need to add a method addHotel in ColorPropertyTile class to add the number of hotel.
             String color = property.getColor();
             switch (color) {
                 case "Brown":
