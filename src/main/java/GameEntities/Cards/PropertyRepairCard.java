a784a8529e6cb4b5daae8f30c0efffadc2c8a9fa
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-13 00:57:26 -0500

    The PropertyRepairCard is a Card subclass that takes away money from the Player based off  the amount of hotels and houses they have. This required a separate subclass from MoneyCard because it's method is very different and it required a couple different instance attributes.

---
+++ src/main/java/GameEntities/Cards/PropertyRepairCard.java
@@ -0,0 +1,32 @@
+package GameEntities.Cards;
+
+import GameEntities.Player;
+
+import GameEntities.Tiles.Property;
+
+import java.util.ArrayList;
+
+public class PropertyRepairCard extends Card{
+    private int houseRepair;
+    private int hotelRepair;
+
+    protected PropertyRepairCard(String cardName, String cardDisplayName, boolean ownable,
+                                 String flavourText, boolean chanceCard, int houseRepair, int hotelRepair) {
+        super(cardName, cardDisplayName, flavourText, ownable, chanceCard);
+        this.hotelRepair = hotelRepair;
+        this.houseRepair = houseRepair;
+    }
+
+    @Override
+    public void cardAction(Player player) {
+        int totalHouses = 0;
+        int totalHotels = 0;
+        ArrayList<Property> pProperties = player.getProperties();
+        for(int i = 0; i < pProperties.size(); i++){
+            totalHouses = totalHouses + pProperties.get(i).getHouses();
+            totalHotels = totalHotels + pProperties.get(i).getHotels();
+        }
+        player.addMoney(totalHotels * -hotelRepair);
+        player.addMoney(totalHouses * -houseRepair);
+    }
+}
