ec1b57882cc8976bdada48d639a49276fa8ecf12
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-13 01:00:06 -0500

    The MoneyCard is the most simple card subclass as it it's only function is to add/deduct money from a Player

---
+++ src/main/java/GameEntities/Cards/MoneyCard.java
@@ -0,0 +1,20 @@
+package GameEntities.Cards;
+
+import GameEntities.Player;
+
+public class MoneyCard extends Card{
+    private int money; //This number can be positive or negative depending on whether you gain
+    //or lose money
+    protected MoneyCard(String cardName, String cardDisplayName, String flavourText, boolean ownable,
+                        boolean chanceCard, int money) {
+        super(cardName, cardDisplayName, flavourText, ownable, chanceCard);
+
+        this.money = money;
+    }
+
+
+    @Override
+    public void cardAction(Player player) {
+        player.addMoney(this.money);
+    }
+}
