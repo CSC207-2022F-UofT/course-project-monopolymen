4d7d4b8a797c3d68cf900b2f66b2c384b3f0bb75
alirezaazim <alirezasend@gmail.com>
2022-11-14 14:24:11 -0500

    added comments and made some minor updates to the methods

--- src/main/java/GameEntities/Player.java
+++ src/main/java/GameEntities/Player.java
@@ -1,12 +1,13 @@
 package GameEntities;
-
+import GameEntities.Tiles.*;
 import java.util.ArrayList;
 /**
- *
+ * A game of monopoly will have 4 players each responsible for keeping track of information such as
+ * their money, position and so on...
  */
 public class Player {
     private String name;
-    private int money = 1500;
+    private int money;
     private ArrayList<Property> properties;
     private int turnsInJail = -1;
     private int position = 0;
@@ -16,9 +17,19 @@ public class Player {
 
     private int getOutOfJailFree = 0;
 
-    public Player(String nameInput, String iconInput){
+    /**
+     * The Player can choose their name and icon but the other attributes are set to default values
+     *
+     * @param iconInput     string representing the icon the player has chosen to be
+     *
+     * @param nameInput     string representing the name the player has chosen
+     *
+     * @param money         int representing the amount of money a player starts off with
+     */
+    public Player(String nameInput, String iconInput, int money){
         this.name = nameInput;
         this.icon = iconInput;
+        this.money = money;
     }
 
     //getters:
@@ -56,7 +67,13 @@ public class Player {
     }
 
     //setters:
-
+    /**
+     * Set the last roll of the player so that it can be used for actions that might happen during the turn
+     *
+     * @param roll1         int representing the value of the first dice
+     *
+     * @param roll2         int representing the value of the second dice
+     */
     public void setLastRoll(int roll1, int roll2){
         this.lastRoll[0] = roll1;
         this.lastRoll[2] = roll2;
@@ -64,10 +81,21 @@ public class Player {
 
     //methods:
 
+    /**
+     * Add an amount of money to the players total
+     * @param add       int representing the amount of money that we are trying to add
+     */
     public void addMoney(int add){
         this.money += add;
     }
 
+
+    /**
+     * Attempt to subtract an amount of money from the players total
+     *
+     * @param subtract      int representing the amount of money we are trying to subtract
+     * @return              return true if the player had enough money to make the payment and false if they do not
+     */
     public boolean subtractMoney(int subtract){
         if(this.money >= subtract){
             this.money -= subtract;
@@ -76,51 +104,100 @@ public class Player {
         return false;
     }
 
+    /**
+     * Check if a player owns a property
+     *
+     * @param check     Property that we are trying to check if the player owns
+     * @return          True if the player owns property and false if they do not
+     */
+
     public boolean ownsProperty(Property check){
         return this.properties.contains(check);
     }
 
+    /**
+     * Add a property to a players property ArrayList
+     *
+     * @param add       Property that we are trying to add to the players owned properties ArrayList
+     */
     public void addProperty(Property add){
         this.properties.add(add);
     }
 
+    /**
+     * Remove a given property from a players property ArrayList
+     *
+     * @param sell      Property that the player is trying to sell
+     */
     public void sellProperty(Property sell){
         this.properties.remove(sell);
     }
 
+    /**
+     * This method will be called every turn a player is in the jail to update the in jail turn tracker
+     */
     public void addTurnInJail(){
-        //do we set this to 0 or to 1 when the player first goes to jail
         this.turnsInJail += 1;
     }
 
+    /**
+     * Called when the player has left the jail to reset the number of turn spent in jail to -1 which just states that
+     * the player is not in jail
+     */
     public void resetTurnInJail(){
         this.turnsInJail = -1;
     }
 
+    /**
+     * Called when a player enters the jail, updates the position of the player to the jail position and adds a turn in
+     * jail
+     */
+    public void enterJail(){
+        this.addTurnInJail();
+        this.position = Board.getJailTilePosition();
+    }
+
+    /**
+     * Update the position of a player given the sum of the dies they have rolled
+     *
+     * @param rollSum       the sum of the two dies the player has rolled
+     */
     public void updatePosition(int rollSum) {
-        if (this.position + rollSum <= 29) {
+        if (this.position + rollSum <= 39) {
             this.position += rollSum;
-        } else if (this.position + rollSum == 30) {
+        } else if (this.position + rollSum == 40) {
             this.position = 0;
         } else{
-            rollSum = rollSum - (30 - this.position);
+            rollSum = rollSum - (40 - this.position);
             this.position = rollSum;
         }
     }
 
+    /**
+     * Checks if a player has a get out of jail free card
+     *
+     * @return      True if the player owns a get out of jail free card
+     */
     public boolean getGetOutOfJailCard(){
         return (this.getOutOfJailFree != 0);
     }
 
+    /**
+     * Remove 1 get out of jail free card from the players inventory
+     */
     public void removeGetOutOfJailCard(){
         this.getOutOfJailFree -= 1;
     }
 
-    public void updateConsecutiveDoubles(boolean doubleRoll){
-        if(doubleRoll){
+    /**
+     * Take in a players two die rolls and update the consecutive doubles attributes if they are equal
+     *
+     * @param roll1     int representing the value of the first dice roll
+     * @param roll2     int representing the value of the second dice roll
+     */
+    public void updateConsecutiveDoubles(int roll1, int roll2){
+        if(roll1 == roll2){
             this.consecutiveDoubles += 1;
-        }else{
-            this.consecutiveDoubles = 0;
         }
     }
 }
