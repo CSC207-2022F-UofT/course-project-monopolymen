6388b9e0484536203f70107d683a0cbfa00a4113
iamyanbo <yanbocheng01234@gmail.com>
2022-11-14 14:31:11 -0500

    Update MovePlayerUseCase to include implemented methods of MovePlayerInputBoundary

--- src/main/java/TurnUseCases/MovePlayerOutputBoundary.java
+++ src/main/java/TurnUseCases/MovePlayerOutputBoundary.java
@@ -3,7 +3,7 @@ package TurnUseCases;
 public interface MovePlayerOutputBoundary {
     /**
      * @param choicesPlayerCanMake String array of the options the player can make
-     * @param inJail boolean variable for weather the player is in jail
+     * @param inJail Boolean variable for weather the player is in jail
      */
     public void showChoices(String[] choicesPlayerCanMake, boolean inJail);
         /** Important, this method will take the inJail parameter and use it to determine if it should display
--- src/main/java/TurnUseCases/MovePlayerUseCase.java
+++ src/main/java/TurnUseCases/MovePlayerUseCase.java
@@ -3,14 +3,19 @@ import GameEntities.Player;
 import GameEntities.Tiles.TileActionResultModel;
 // Need import here for implemented version of movePlayerOutputBoundary which is in presenter
 
-public class MovePlayerUseCase {
+public class MovePlayerUseCase implements MovePlayerInputBoundary {
     /**
      *
      * @param playerRollAmount Int array of length 2 representing the roll of two die
      * @param player The player object that the action is being performed on
-     * @param movePlayerOutputBoundary movePlayer output boundary to handle the connection to the turn presenter
-     * @param board board the game is operating on
+     * @param movePlayerPresenter Implemented movePlayerOutputBoundary to handle the connection to the turn presenter
+     * @param board The board the game is operating on
      */
+    public void makePlayerChoice(int[] playerRollAmount, Player player, MovePlayerUseCase movePlayerUseCase,
+                                 MovePlayerPresenter movePlayerPresenter) {
+        startAction(playerRollAmount, player, movePlayerPresenter, board);
+    }
+
     public void startAction(int[] playerRollAmount, Player player, MovePlayerPresenter movePlayerPresenter,
                             Board board) {
         int rollSum = playerRollAmount[0] + playerRollAmount[1];
