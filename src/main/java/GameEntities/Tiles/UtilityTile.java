9660c7e44aa0708be1838bd64711f67eee46ae7f
Joshua Lenander <42joshl@gmail.com>
2022-11-14 23:05:11 -0500

    Fix UtilityTile so it compiles. Add action and getRent methods. Change rentPrice to multiplicative factor rentFactor.

--- src/main/java/GameEntities/Tiles/UtilityTile.java
+++ src/main/java/GameEntities/Tiles/UtilityTile.java
@@ -1,12 +1,21 @@
 package GameEntities.Tiles;
 
-public class UtilityTile extends Property{
-    private final int[] rentPrice;
+import GameEntities.Player;
+
+import java.util.List;
+
+import static java.lang.Math.min;
+
+public class UtilityTile extends Property {
+    private final int[] rentFactor;
 
     /**
      * Construct a Utility Property Tile.
      * When landed on, players can choose to buy the utility for its printed price.
      *
+     * @param rentFactor          An array specifying the multiplicative factor where the index denotes the number
+     *                            of utilities owned besides this one (ex. rentFactor[1] is the rate that the dice
+     *                            roll is multiplied by to determine the rent paid).
      * @param propertyName        The internal string name representing this tile
      *                            (intended to not contain spaces or other special characters).
      * @param propertyDisplayName The string name displayed to the user. This may have special characters.
@@ -15,9 +24,67 @@ public class UtilityTile extends Property{
      * @param unMortgageValue     The value of this property for unMortgaging purposes
      * @see GameEntities.Tiles.Tile
      */
-    public UtilityTile(int[] rentPrice, String propertyName, String propertyDisplayName, int purchasePrice,
+    public UtilityTile(int[] rentFactor, String propertyName, String propertyDisplayName, int purchasePrice,
                        int mortgageValue, int unMortgageValue) {
         super(propertyName, propertyDisplayName, purchasePrice, mortgageValue, unMortgageValue);
-        this.rentPrice = rentPrice;
+        this.rentFactor = rentFactor;
+    }
+
+    /**
+     * Returns the number of utilities that the owner of this Property owns in propertyList.
+     * Does not count this property if it is not in propertyList
+     * If there is no owner, the return value is 0.
+     *
+     * @param propertyList The list of Property objects to search through
+     * @return The number of utilities that this Property's owner owns in total. 0 if no owner of this Property.
+     */
+    public int numUtilityOwned(List<Property> propertyList) {
+        if (!isOwned()) {
+            return 0;
+        }
+
+        int numOwned = 0;
+        for (Property property : propertyList) {
+            if (property instanceof UtilityTile &&
+                    property.isOwned() &&
+                    property.getOwner().equals(getOwner())) {
+                numOwned++;
+            }
+        }
+        return numOwned;
+    }
+
+    /**
+     * Get the Rent value for this Utility.
+     * Rent value is a multiplicative factor (depending on the number of utilities owned) times the dice roll from
+     * rentPayer's last roll.
+     *
+     * @param rentPayer    The Player who is paying the rent.
+     * @param propertyList The list of properties to search for other utilities in.
+     * @return The int rent value of this property
+     */
+    @Override
+    public int getRent(Player rentPayer, List<Property> propertyList) {
+        int numOwned = numUtilityOwned(propertyList);
+        // If we have the unintended effect of more railroads owned than accounted for, return the last rent value.
+        numOwned = min(numOwned - 1, rentFactor.length - 1);
+
+        int diceRollSum = 0;
+        for (int value : rentPayer.getLastRoll()) {
+            diceRollSum += value;
+        }
+
+        return rentFactor[numOwned] * diceRollSum;
+    }
+
+    /**
+     * Perform the action for when Player <i>player</i> lands on this tile.
+     *
+     * @param player The Player that the action is being performed on (landed on the tile)
+     * @return A TileActionResultModel object describing the action that was performed
+     */
+    @Override
+    public TileActionResultModel action(Player player) {
+        return null;
     }
 }
