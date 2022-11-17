df45208e368ada160b511371df16e6d2d798991c
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-11-17 18:13:51 -0500

    Adding input and output boundary.

---
+++ src/main/java/TurnUseCases/MortgageUseCase/MortgagePropertyOutputBoundary.java
@@ -0,0 +1,49 @@
+package TurnUseCases.MortgageUseCase;
+
+import GameEntities.Tiles.ColorPropertyTile;
+import GameEntities.Tiles.Property;
+/**
+ * The output boundary for the mortgage property use case.
+ */
+public interface MortgagePropertyOutputBoundary {
+    /**
+     * Presents the list of players player can trade with.
+     *
+     * @param players the player who wants to mortgage or unmortgage property.
+     * @param flavorText the text describing what is happening.
+     */
+    public void showPlayer(String players, String flavorText);
+
+    /**
+     * Presents the property which is mortgaged by player.
+     *
+     * @param property The property which is mortgaged by player.
+     * @param flavorText the text describing what is happening.
+     */
+    public void showMortgageProperty(Property property, String flavorText);
+
+    /**
+     * Presents Tthe property which is mortgaged by player.
+     *
+     * @param property The property which is mortgaged by player.
+     * @param flavorText the text describing what is happening.
+     */
+    public void showMortgageProperty(ColorPropertyTile property, String flavorText);
+
+    /**
+     * Presents the property which is unmortgaged by player.
+     *
+     * @param property details the possible options for the trade
+     * @param flavorText the text describing what is happening.
+     */
+    public void showUnmortgageProperty(Property property, String flavorText);
+
+    /**
+     * Presents the property which is unmortgaged by player.
+     *
+     * @param property details the possible options for the trade
+     * @param flavorText the text describing what is happening.
+     */
+    public void showUnmortgageProperty(ColorPropertyTile property, String flavorText);
+
+}
