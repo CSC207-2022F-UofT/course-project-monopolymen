b0ddb0952f047e434fc19c343b53bfd08c84bd9f
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-11-17 18:13:51 -0500

    Adding input and output boundary.

---
+++ src/main/java/TurnUseCases/MortgageUseCase/MortgagePropertyInputBoundary.java
@@ -0,0 +1,33 @@
+package TurnUseCases.MortgageUseCase;
+
+import GameEntities.Player;
+import GameEntities.Tiles.ColorPropertyTile;
+import GameEntities.Tiles.Property;
+/**
+ * The input boundary for the mortgage property use case.
+ */
+public interface MortgagePropertyInputBoundary {
+    /**
+     * Player mortgages property
+     *
+     * @param player the player who wants mortgages property.
+     * @param property the property which player wants to mortgages.
+     */
+    public void mortgage(Player player, Property property);
+
+    /**
+     * Overloading method mortgage. Player mortgages a color property.
+     *
+     * @param player the player who wants mortgages property.
+     * @param property the property which player wants to mortgages.
+     */
+    public void mortgage(Player player, ColorPropertyTile property);
+
+    /**
+     * Player unmortgages property
+     *
+     * @param player the player who wants to unmortgages property.
+     * @param property the property which player wants to unmortgages.
+     */
+    public void unmortgage(Player player, Property property);
+}
