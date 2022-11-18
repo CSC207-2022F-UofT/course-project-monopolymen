14d92f26e024afb6eb938c0ef221f3c0e71603ef
Joshua Lenander <42joshl@gmail.com>
2022-11-17 21:20:40 -0500

    Clarify passing rules in javadoc

--- src/main/java/GameEntities/Tiles/TilePassResultModel.java
+++ src/main/java/GameEntities/Tiles/TilePassResultModel.java
@@ -2,8 +2,9 @@ package GameEntities.Tiles;
 
 /**
  * Data Model for the result of a player passing a tile.
- * Players that land on a tile are also "Passing" the tile.
- * Generally nothing happens when a player passes a tile.
+ * Players that land on a tile are also "Passing" the tile. (The passing method will be called for a tile before
+ * the player lands on that tile).
+ * Generally nothing happens when a player passes a tile, the exception being the Go Tile which functions on passing.
  *
  * @see GameEntities.Tiles.Tile
  */
@@ -14,7 +15,8 @@ public class TilePassResultModel {
 
     /**
      * Construct an object that contains data for the result of passing a game tile.
-     * Players that land on a tile are also "Passing" the tile.
+     * Players that land on a tile are also "Passing" the tile. (The passing method will be called for a tile before
+     * the player lands on that tile).
      *
      * @param actionTaken whether an action was taken when the player passed the tile
      * @param flavorText  the text describing the action's result
