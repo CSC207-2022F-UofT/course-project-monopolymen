bc22b278900dbe664ec8083b40decb14ced383cc
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-21 15:45:22 -0500

    Changed constructors to public

--- src/main/java/GameEntities/Cards/GetOutOfJailCard.java
+++ src/main/java/GameEntities/Cards/GetOutOfJailCard.java
@@ -2,7 +2,7 @@ package GameEntities.Cards;
 import GameEntities.Player;
 
 public class GetOutOfJailCard  extends Card{
-    protected GetOutOfJailCard(String cardName, String cardDisplayName, String flavourText, boolean ownable, boolean chanceCard) {
+    public GetOutOfJailCard(String cardName, String cardDisplayName, String flavourText, boolean ownable, boolean chanceCard) {
         super(cardName, cardDisplayName, flavourText, chanceCard);
     }
 
