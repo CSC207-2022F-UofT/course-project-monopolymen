c6cf79bd9bd9130de0107a1c88233c09f2b8cd4e
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-11-21 15:11:12 -0500

    Adding selling method

--- src/main/java/TurnUseCases/BuildUseCase/BuildBuildings.java
+++ src/main/java/TurnUseCases/BuildUseCase/BuildBuildings.java
@@ -17,7 +17,7 @@ public class BuildBuildings implements BuildBuildingInputBoundary{
     /**
      * Check if player can build buildings
      *
-     * @param player the player who wants build buildings.
+     * @param player the player who wants  to build buildings.
      * @param property the property on which player wants to build buildings.
      */
     @Override
@@ -74,7 +74,7 @@ public class BuildBuildings implements BuildBuildingInputBoundary{
     /**
      * Build a house on the property.
      *
-     * @param player the player who wants build buildings.
+     * @param player the player who wants to build buildings.
      * @param property the property on which player wants to build buildings.
      * @param presenter presenter which provides info for players.
      */
@@ -84,24 +84,7 @@ public class BuildBuildings implements BuildBuildingInputBoundary{
             property.addHouse();
             //need to add a method addHouse in ColorPropertyTile class to add the number of house.
             String color = property.getColor();
-            switch (color) {
-                case "Brown":
-                case "Light Blue":
-                    player.subtractMoney(50);
-                    break;
-                case "Pink":
-                case "Orange":
-                    player.subtractMoney(100);
-                    break;
-                case "Red":
-                case "Yellow":
-                    player.subtractMoney(150);
-                    break;
-                case "Green":
-                case "Dark Blue":
-                    player.subtractMoney(200);
-                    break;
-            }
+            player.subtractMoney(property.getBuildingCost);
             String text = player.getName() + "built a house on " + property.getTileName();
             presenter.showBuildBuilding(player, property, text);
         }
@@ -112,7 +95,7 @@ public class BuildBuildings implements BuildBuildingInputBoundary{
     /**
      * Build a hotel on the property.
      *
-     * @param player the player who wants build buildings.
+     * @param player the player who wants to build buildings.
      * @param property the property on which player wants to build buildings.
      * @param presenter presenter which provides info for players.
      */
@@ -122,28 +105,92 @@ public class BuildBuildings implements BuildBuildingInputBoundary{
             property.addHotel();
             //need to add a method addHotel in ColorPropertyTile class to add the number of hotel.
             String color = property.getColor();
-            switch (color) {
-                case "Brown":
-                case "Light Blue":
-                    player.subtractMoney(50);
-                    break;
-                case "Pink":
-                case "Orange":
-                    player.subtractMoney(100);
-                    break;
-                case "Red":
-                case "Yellow":
-                    player.subtractMoney(150);
-                    break;
-                case "Green":
-                case "Dark Blue":
-                    player.subtractMoney(200);
-                    break;
-            }
+            player.subtractMoney(property.getBuildingCost);
             String text = player.getName() + "built a hotel on " + property.getTileName();
             presenter.showBuildBuilding(player, property, text);
         }
         String text = player.getName() + "cannot build a hotel on " + property.getTileName();
         presenter.showBuildBuilding(player, property, text);
+    }
+
+    /**
+     * Check if player can sell buildings
+     *
+     * @param player the player who wants to sell buildings.
+     * @param property the property on which player wants to sell buildings.
+     */
+    @Override
+    public boolean isSellable(Player player, Property property) {
+        if(!player.ownsProperty(property)){
+            return false;
+        }
+        if(!property.getClass().getName().equals("GameEntities.Tiles.ColorPropertyTile")){
+            return false;
+        }
+        ArrayList<ColorPropertyTile> ColorProperties = new ArrayList<ColorPropertyTile>();
+        ArrayList<Property> properties = player.getProperties();
+        for (int i = 0; i < properties.size(); i++){
+            if(properties.get(i).getClass().getName().equals("GameEntities.Tiles.ColorPropertyTile")){
+                ColorProperties.add(properties.get(i));
+            }
+        }
+        // Selecting ColorPropertyTile with the same color.
+        String color = property.getColor();
+        ArrayList<ColorPropertyTile> SameColorProperties = new ArrayList<ColorPropertyTile>();
+        for (int i = 0; i < ColorProperties.size(); i++){
+            if(ColorProperties.get(i).getColor().equals(color)) {
+                SameColorProperties.add(ColorProperties.get(i));
+            }
+        }
+        for (ColorPropertyTile sameColorProperty : SameColorProperties) {
+            int a = sameColorProperty.getHouse() + sameColorProperty.getHotel();
+            int b = property.getHouse() + property.getHotel();
+            if (b < a) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Selling a house on the property.
+     *
+     * @param player the player who wants to sell buildings.
+     * @param property the property on which player wants to sell buildings.
+     * @param presenter presenter which provides info for players
+     */
+    @Override
+    public void sellHouse(Player player, ColorPropertyTile property, BuildBuildingOutputBoundary presenter){
+        if (isSellable(player, property) && property.getHotel() == 0){
+            property.subtractHouse();
+            int value = 0;
+            value = 0.5*property.getBuildingCost;
+            player.addMoney(value);
+            String text = player.getName() + "sold a house on " + property.getTileName();
+            presenter.showSellBuilding(player, property, text);
+        }
+        String text = player.getName() + "cannot sell a house on " + property.getTileName();
+        presenter.showSellBuilding(player, property, text);
+    }
+
+    /**
+     * Selling a hotel on the property.
+     *
+     * @param player the player who wants to sell buildings.
+     * @param property the property on which player wants to sell buildings.
+     * @param presenter presenter which provides info for players
+     */
+    @Override
+    public void sellHotel(Player player, ColorPropertyTile property, BuildBuildingOutputBoundary presenter){
+        if (isSellable(player, property) && property.getHotel() > 0){
+            property.subtractHotel();
+            int value = 0;
+            value = 0.5*property.getBuildingCost;
+            player.addMoney(value);
+            String text = player.getName() + "sold a hotel on " + property.getTileName();
+            presenter.showSellBuilding(player, property, text);
+        }
+        String text = player.getName() + "cannot sell a hotel on " + property.getTileName();
+        presenter.showSellBuilding(player, property, text);
     }
 }
