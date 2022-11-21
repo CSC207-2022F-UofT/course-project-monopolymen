4844c34be0b06d19b3c13e538e2a76db45ef9fe1
alirezaazim <alirezasend@gmail.com>
2022-11-21 16:01:19 -0500

    renamed factory class and added testing

--- src/main/java/GameEntities/Tiles/FactoryProperty.java
+++ src/main/java/GameEntities/Tiles/FactoryProperty.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Factory {
+public class FactoryProperty {
     /**
      * Extract and put each property into an ArrayList
      *
