bbadfd4833642ba4ac68eee41f4a7fd1dcde4175
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-25 13:27:24 -0500

    Changed the constructor of board so that it makes empty lists of Cards instead of taking it in as a parameter. Added two methods that add chance/community cards to the lists.

--- src/main/java/GameEntities/Board.java
+++ src/main/java/GameEntities/Board.java
@@ -5,55 +5,63 @@ import java.util.ArrayList;
 import GameEntities.Tiles.JailTile;
 import GameEntities.Tiles.Tile;
 import GameEntities.Tiles.Property;
-import GameEntities.Cards;
+import GameEntities.Cards.Card;
 
 public class Board {
     private  ArrayList<Card> communityCards;
     private ArrayList<Card> chanceCards;
     private ArrayList<Tile> tilesList;
-    Board(ArrayList<Card> communityCards, ArrayList<Card> chanceCards, ArrayList<Tile> tilesList) {
+    public Board(ArrayList<Tile> tilesList) {
+        ArrayList<Card> communityCards = new ArrayList<>();
+        ArrayList<Card> chanceCards = new ArrayList<>();
+
+
         this.communityCards = communityCards;
         this.chanceCards = chanceCards;
         this.tilesList = tilesList;
     }
 
+    public void addCommunityCard(Card communityCard){communityCards.add(communityCard);}
+    public void addChanceCard(Card chanceCard){chanceCards.add(chanceCard);}
 
-        public int getTilePosition(String tileName){
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tileName.equals(tilesList.get(i).getTileName())){
-                    return i;
-                }
+
+
+    public int getTilePosition(String tileName){
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tileName.equals(tilesList.get(i).getTileName())){
+                return i;
             }
-            return 0;
         }
-
-        public ArrayList<Tile> getPropertyTiles(){
-            ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof Property){
-                    propertyTiles.add(tilesList.get(i));
-                }
-            }
-            return propertyTiles;
-        }
-
-        public ArrayList<Tile> getTilesList() {
-            return tilesList;
-        }
-
-        public Tile getTile(int tilePosition){
-            return this.tilesList.get(tilePosition);
-        }
-
-        public int getJailTilePosition(){
-            int jailTilePosition = 0;
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof JailTile){
-                    jailTilePosition = i;
-                }
-            }
-
-            return jailTilePosition;
-        }
-
+        return 0;
     }
+
+    public ArrayList<Tile> getPropertyTiles(){
+        ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof Property){
+                propertyTiles.add(tilesList.get(i));
+            }
+        }
+        return propertyTiles;
+    }
+
+    public ArrayList<Tile> getTilesList() {
+        return tilesList;
+    }
+
+    public Tile getTile(int tilePosition){
+        return this.tilesList.get(tilePosition);
+    }
+
+    public int getJailTilePosition(){
+        int jailTilePosition = 0;
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof JailTile){
+                jailTilePosition = i;
+            }
+        }
+
+        return jailTilePosition;
+    }
+
+}
