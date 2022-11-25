83aea1c9ca36bcb9f207a363936912db03d0c108
Yanbo Cheng <86802756+iamyanbo@users.noreply.github.com>
2022-11-24 19:46:13 -0500

    Revert "Update GoToJailTile"

--- src/main/java/GameEntities/Tiles/GoToJailTile.java
+++ src/main/java/GameEntities/Tiles/GoToJailTile.java
@@ -18,7 +18,7 @@ public class GoToJailTile extends Tile{
      */
     @Override
     public TileActionResultModel action(Player player) {
-        player.enterJail();
+        player.addTurnInJail();
         return new TileActionResultModel("You are being sent to jail.", player, -1);
     }
 }
--- src/main/java/GameEntities/Tiles/JailTile.java
+++ src/main/java/GameEntities/Tiles/JailTile.java
@@ -5,6 +5,12 @@ import GameEntities.Player;
 public class JailTile extends Tile{
     /**
      * A player landing on the space cannot move for 3 turns unless they pay or have "get out of jail" card.
+     *
+     * @param tileName        The internal string name representing this tile
+     *                        (intended to not contain spaces or other special characters).
+     * @param tileDisplayName The string name displayed to the user. This may have special characters.
+     * @param ownable         Whether the tile is able to be owned by a player. This is protected with the intention that
+     *                        only subclasses of Tile can be ownable.
      * @see GameEntities.Tiles.Property
      */
     public JailTile() {
