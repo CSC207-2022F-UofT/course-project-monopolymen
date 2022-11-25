6f45ccd7c74baa6f0f08bc8835edfc74b17c5876
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-25 13:37:19 -0500

    Changed the constructor of board so that it makes empty lists of Cards instead of taking it in as a parameter. Added two methods that add chance/community cards to the lists.

--- src/main/java/GameEntities/Board.java
+++ src/main/java/GameEntities/Board.java
@@ -11,11 +11,18 @@ public class Board {
     private  ArrayList<Card> communityCards;
     private ArrayList<Card> chanceCards;
     private ArrayList<Tile> tilesList;
-     public Board(ArrayList<Card> communityCards, ArrayList<Card> chanceCards, ArrayList<Tile> tilesList) {
+    public Board(ArrayList<Tile> tilesList) {
+        ArrayList<Card> communityCards = new ArrayList<>();
+        ArrayList<Card> chanceCards = new ArrayList<>();
+
+
         this.communityCards = communityCards;
         this.chanceCards = chanceCards;
         this.tilesList = tilesList;
     }
+
+    public void addCommunityCard(Card communityCard){communityCards.add(communityCard);}
+    public void addChanceCard(Card chanceCard){chanceCards.add(chanceCard);}
 
 
         public int getTilePosition(String tileName){
