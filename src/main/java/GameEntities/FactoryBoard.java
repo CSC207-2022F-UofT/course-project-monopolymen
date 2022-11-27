5f7bc0873cdebf962c255f2913dd46df4fc7e501
alirezaazim <alirezasend@gmail.com>
2022-11-26 20:35:35 -0500

    added factoryboard and factorycard

---
+++ src/main/java/GameEntities/FactoryBoard.java
@@ -0,0 +1,12 @@
+package GameEntities;
+
+/** The goal of this class is to create a Board.
+ *  There are 4 required CSV files to create a board,
+ *  1) ColoredProperty CSV
+ *  2) RailRoadProperty CSV
+ *  3) UtilityProperty CSV
+ *  4) Community/Chance Card CSV
+ */
+public class FactoryBoard {
+
+}
