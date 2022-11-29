75978d29c01d8344e92bad7d974621c68fe00fff
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-28 23:31:03 -0500

    Changed constructor of DrawCardTile so that it's first created with an empty board and added a setter for the Board. Edited Board.java as for some reason the methods had an extra indent

--- src/main/java/GameEntities/Board.java
+++ src/main/java/GameEntities/Board.java
@@ -26,62 +26,62 @@ public class Board {
     public void addChanceCard(Card chanceCard){chanceCards.add(chanceCard);}
 
 
-        public int getTilePosition(String tileName){
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tileName.equals(tilesList.get(i).getTileName())){
-                    return i;
-                }
-            }
-            return 0;
-        }
-
-        public ArrayList<Tile> getPropertyTiles(){
-            ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof Property){
-                    propertyTiles.add(tilesList.get(i));
-                }
-            }
-            return propertyTiles;
-        }
-
-        public ArrayList<Tile> getTilesList() {
-            return tilesList;
-        }
-
-        public Tile getTile(int tilePosition){
-            return this.tilesList.get(tilePosition);
-        }
-
-        public int getJailTilePosition(){
-            int jailTilePosition = 0;
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof JailTile){
-                    jailTilePosition = i;
-                }
-            }
-
-            return jailTilePosition;
-        }
-
-        public Card pickCard(boolean chanceCard){
-            if(chanceCard){
-                //chooses a card off the top
-                Card returnCard = chanceCards.get(0);
-                //moves the card to the back of the deck
-                chanceCards.remove(0);
-                chanceCards.add(returnCard);
-
-                return returnCard;
-
-            }
-
-            else{
-                Card returnCard = communityCards.get(0);
-                communityCards.remove(0);
-                communityCards.add(returnCard);
-                return returnCard;
+    public int getTilePosition(String tileName){
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tileName.equals(tilesList.get(i).getTileName())){
+                return i;
             }
         }
-
+        return 0;
     }
+
+    public ArrayList<Tile> getPropertyTiles(){
+        ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof Property){
+                propertyTiles.add(tilesList.get(i));
+            }
+        }
+        return propertyTiles;
+    }
+
+    public ArrayList<Tile> getTilesList() {
+        return tilesList;
+    }
+
+    public Tile getTile(int tilePosition){
+        return this.tilesList.get(tilePosition);
+    }
+
+    public int getJailTilePosition(){
+        int jailTilePosition = 0;
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof JailTile){
+                jailTilePosition = i;
+            }
+        }
+
+        return jailTilePosition;
+    }
+
+    public Card pickCard(boolean chanceCard){
+        if(chanceCard){
+            //chooses a card off the top
+            Card returnCard = chanceCards.get(0);
+            //moves the card to the back of the deck
+            chanceCards.remove(0);
+            chanceCards.add(returnCard);
+
+            return returnCard;
+
+        }
+
+        else{
+            Card returnCard = communityCards.get(0);
+            communityCards.remove(0);
+            communityCards.add(returnCard);
+            return returnCard;
+        }
+    }
+
+}
--- src/main/java/GameEntities/Tiles/DrawCardTile.java
+++ src/main/java/GameEntities/Tiles/DrawCardTile.java
@@ -4,15 +4,24 @@ import GameEntities.Cards.Card;
 import GameEntities.Player;
 import GameEntities.Board;
 
+import java.util.ArrayList;
+
 
 public class DrawCardTile extends Tile{
 
     private final boolean chanceTile;
-    private final Board board;
+    private Board board;
 
-    public DrawCardTile(String tileName, String tileDisplayName, boolean chanceTile, Board board) {
+    public DrawCardTile(String tileName, String tileDisplayName, boolean chanceTile) {
         super(tileName, tileDisplayName);
         this.chanceTile = chanceTile;
+
+        ArrayList<Tile> tileList  = new ArrayList<>();
+        Board board = new Board(tileList);
+        this.board = board;
+    }
+
+    public void setBoard(Board board){
         this.board = board;
     }
 
