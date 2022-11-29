66074cdd6588d48732b15d6a385e8dab1893c217
TahirLeonenko <tahir.leonenko@gmail.com>
2022-11-28 23:39:52 -0500

    Changed constructor of DrawCardTile so that it's first created with an empty board and added a setter for the Board. Edited Board.java as for some reason the methods had an extra indent

--- src/main/java/GameEntities/Board.java
+++ src/main/java/GameEntities/Board.java
@@ -11,11 +11,11 @@ public class Board {
     private final ArrayList<Card> communityCards;
     private final ArrayList<Card> chanceCards;
     private final ArrayList<Tile> tilesList;
-    
+
     public Board(ArrayList<Tile> tilesList) {
         ArrayList<Card> communityCards = new ArrayList<>();
         ArrayList<Card> chanceCards = new ArrayList<>();
-        
+
         this.communityCards = communityCards;
         this.chanceCards = chanceCards;
         this.tilesList = tilesList;
@@ -26,61 +26,62 @@ public class Board {
     public void addChanceCard(Card chanceCard){chanceCards.add(chanceCard);}
 
 
-        public int getTilePosition(String tileName){
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tileName.equals(tilesList.get(i).getTileName())){
-                    return i;
-                }
-            }
-            return 0;
-        }
-
-        public ArrayList<Tile> getPropertyTiles(){
-            ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof Property){
-                    propertyTiles.add(tilesList.get(i));
-                }
-            }
-            return propertyTiles;
-        }
-
-        public ArrayList<Tile> getTilesList() {
-            return tilesList;
-        }
-
-        public Tile getTile(int tilePosition){
-            return this.tilesList.get(tilePosition);
-        }
-
-        public int getJailTilePosition(){
-            int jailTilePosition = 0;
-            for(int i = 0; i < tilesList.size(); i++){
-                if(tilesList.get(i) instanceof JailTile){
-                    jailTilePosition = i;
-                }
-            }
-
-            return jailTilePosition;
-        }
-
-        public Card pickCard(boolean chanceCard){
-            if(chanceCard){
-                //chooses a card off the top and removes it from the deck
-                Card returnCard = chanceCards.remove(0);
-                //moves the card to the back of the deck
-                chanceCards.add(returnCard);
-
-                return returnCard;
-
-            }
-
-            else{
-                Card returnCard = communityCards.get(0);
-                communityCards.remove(0);
-                communityCards.add(returnCard);
-                return returnCard;
+    public int getTilePosition(String tileName){
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tileName.equals(tilesList.get(i).getTileName())){
+                return i;
             }
         }
-
+        return 0;
     }
+
+    public ArrayList<Tile> getPropertyTiles(){
+        ArrayList<Tile> propertyTiles = new ArrayList<Tile>();
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof Property){
+                propertyTiles.add(tilesList.get(i));
+            }
+        }
+        return propertyTiles;
+    }
+
+    public ArrayList<Tile> getTilesList() {
+        return tilesList;
+    }
+
+    public Tile getTile(int tilePosition){
+        return this.tilesList.get(tilePosition);
+    }
+
+    public int getJailTilePosition(){
+        int jailTilePosition = 0;
+        for(int i = 0; i < tilesList.size(); i++){
+            if(tilesList.get(i) instanceof JailTile){
+                jailTilePosition = i;
+            }
+        }
+
+        return jailTilePosition;
+    }
+
+    public Card pickCard(boolean chanceCard){
+        if(chanceCard){
+            //chooses a card off the top
+            Card returnCard = chanceCards.get(0);
+            //moves the card to the back of the deck
+            chanceCards.remove(0);
+            chanceCards.add(returnCard);
+
+            return returnCard;
+
+        }
+
+        else{
+            Card returnCard = communityCards.get(0);
+            communityCards.remove(0);
+            communityCards.add(returnCard);
+            return returnCard;
+        }
+    }
+
+}
