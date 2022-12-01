62d1b4d5f64babe36e7cb6dc264bd358a640d87f
Joshua Lenander <42joshl@gmail.com>
2022-11-30 22:04:33 -0500

    Fix accidental method change.

--- src/main/java/GameEntities/Tiles/GoToJailTile.java
+++ src/main/java/GameEntities/Tiles/GoToJailTile.java
@@ -19,7 +19,7 @@ public class GoToJailTile extends Tile{
      */
     @Override
     public TileActionResultModel action(Player player, Board board) {
-        player.addTurnInJail();
+        player.enterJail();
         return new TileActionResultModel("You are being sent to jail.", player, -1);
     }
 }
