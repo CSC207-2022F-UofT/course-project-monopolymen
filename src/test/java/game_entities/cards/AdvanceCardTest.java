d2ec5a6f5eac1085516644cc151eecce5563947b
TahirLeonenko <tahir.leonenko@gmail.com>
2022-12-01 19:06:31 -0500

    Created tests for cards (this was first commited to Card but the rebase after naming packages made it go wonky)

---
+++ src/test/java/game_entities/cards/AdvanceCardTest.java
@@ -0,0 +1,30 @@
+package game_entities.cards;
+
+import game_entities.Board;
+import game_entities.Player;
+import game_entities.TestObjects;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdvanceCardTest {
+    /* There is only one test here because MovePlayerUseCase is what moves the player in the other
+    AdvanceCard types, so testing will have to be done through that class.
+     */
+    @Test
+    public void cardAction() throws FileNotFoundException {
+        Board testBoard = TestObjects.testBoardReal();
+        AdvanceCard goToJailCard = new AdvanceCard("", "", "",
+                true, -1, "JailTile");
+        Player testPlayer1 = TestObjects.testPlayerRB();
+        goToJailCard.action(testPlayer1);
+        assertEquals(testBoard.getJailTilePosition(), testPlayer1.getPosition());
+
+
+
+    }
+
+
+}
