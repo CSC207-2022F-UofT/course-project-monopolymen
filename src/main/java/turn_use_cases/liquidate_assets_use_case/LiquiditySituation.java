7abe7de82d229345ad9400e9ab1da6ecb1af6374
TahirLeonenko <tahir.leonenko@gmail.com>
2022-12-03 04:42:45 -0500

    Created the LiquidateAssetsInterfaceAdapter class.

--- src/main/java/turn_use_cases/liquidate_assets_use_case/LiquiditySituation.java
+++ src/main/java/turn_use_cases/liquidate_assets_use_case/LiquiditySituation.java
@@ -1,6 +1,7 @@
 package turn_use_cases.liquidate_assets_use_case;
 
 import game.GameState;
+import game_entities.Board;
 import game_entities.Player;
 
 public class LiquiditySituation {
@@ -24,12 +25,19 @@ public class LiquiditySituation {
     }
 
     private final int owedMoney;
-    private GameState gameState;
+    private final GameState gameState;
 
-    public LiquiditySituation(Player affectedPlayer, Player owedPlayer, int owedMoney, GameState gameState) {
+    private final Board board;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public LiquiditySituation(Player affectedPlayer, Player owedPlayer, int owedMoney, GameState gameState, Board board) {
         this.affectedPlayer = affectedPlayer;
         this.owedPlayer = owedPlayer;
         this.owedMoney = owedMoney;
         this.gameState = gameState;
+        this.board = board;
     }
 }
