aa9382e39736993f099e572029f7481df0cd8c22
colewiltse <cole.wiltse@mail.utoronto.ca>
2022-12-05 14:34:54 -0500

    Progress so far making presenter for trade

---
+++ src/main/java/TurnUseCases/TradeUseCase/TestingPresenting.java
@@ -0,0 +1,178 @@
+package TurnUseCases.TradeUseCase;
+import game_entities.Player;
+import turn_use_cases.trade_use_case.TradeOffer;
+import turn_use_cases.trade_use_case.TradeOption;
+import turn_use_cases.trade_use_case.TradeOutputBoundary;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+
+public class TestingPresenting extends JFrame implements TradeOutputBoundary {
+    static final String gapList[] = {"0", "10", "15", "20"};
+    final static int maxGap = 20;
+    JComboBox horGapComboBox;
+    JComboBox verGapComboBox;
+    JButton applyButton = new JButton("Apply gaps");
+    GridLayout experimentLayout = new GridLayout(1,0);
+
+    public TestingPresenting(String name) {
+        super(name);
+        setResizable(false);
+    }
+
+    public void initGaps() {
+        horGapComboBox = new JComboBox(gapList);
+        verGapComboBox = new JComboBox(gapList);
+    }
+
+    public void addComponentsToPane(final Container pane) {
+        initGaps();
+        final JPanel compsToExperiment = new JPanel();
+        compsToExperiment.setLayout(experimentLayout);
+        JPanel controls = new JPanel();
+        controls.setLayout(new GridLayout(2,3));
+
+        //Set up components preferred size
+        JButton b = new JButton("Just fake button");
+        Dimension buttonSize = b.getPreferredSize();
+        compsToExperiment.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 5)+maxGap,
+                (int)(buttonSize.getHeight() * 8)+maxGap * 2));
+
+        //Add buttons to experiment with Grid Layout
+        JButton btn = new JButton("Player 1");
+        compsToExperiment.add(btn);
+        compsToExperiment.add(new JButton("Player 2"));
+        compsToExperiment.add(new JButton("Button 3"));
+        compsToExperiment.add(new JButton("Player 4"));
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test");
+            }
+        });
+
+
+
+        //Add controls to set up horizontal and vertical gaps
+        controls.add(new Label("Horizontal gap:"));
+        controls.add(new Label("Vertical gap:"));
+        controls.add(new Label(" "));
+        controls.add(horGapComboBox);
+        controls.add(verGapComboBox);
+        controls.add(applyButton);
+
+        //Process the Apply gaps button press
+        applyButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                //Get the horizontal gap value
+                String horGap = (String)horGapComboBox.getSelectedItem();
+                //Get the vertical gap value
+                String verGap = (String)verGapComboBox.getSelectedItem();
+                //Set up the horizontal gap value
+                experimentLayout.setHgap(Integer.parseInt(horGap));
+                //Set up the vertical gap value
+                experimentLayout.setVgap(Integer.parseInt(verGap));
+                //Set up the layout of the buttons
+                experimentLayout.layoutContainer(compsToExperiment);
+            }
+        });
+        pane.add(compsToExperiment, BorderLayout.NORTH);
+        pane.add(new JSeparator(), BorderLayout.CENTER);
+        pane.add(controls, BorderLayout.SOUTH);
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method is invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        TestingPresenting frame = new TestingPresenting("GridLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Set up the content pane.
+        frame.addComponentsToPane(frame.getContentPane());
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+
+    /**
+     * Presents the list of players player can trade with.
+     *
+     * @param listOfPlayers the list of players player can trade with.
+     * @param flavorText    the text describing what is happening.
+     */
+    @Override
+    public void showListOfPlayers(ArrayList<Player> listOfPlayers, String flavorText) {
+
+    }
+
+    /**
+     * Presents the possible options for player1 to trade with player2.
+     *
+     * @param tradeOption details the possible options for the trade
+     * @param flavorText  the text describing what is happening.
+     */
+    @Override
+    public void showTradeOptions(TradeOption tradeOption, String flavorText) {
+
+    }
+
+    /**
+     * Presents the trade offer.
+     *
+     * @param tradeOffer details what would happen during the trade.
+     * @param flavorText the text describing what is happening.
+     */
+    @Override
+    public void showTradeOffer(TradeOffer tradeOffer, String flavorText) {
+
+    }
+
+    /**
+     * Presents the result of the trade offer.
+     *
+     * @param option     the number indicating the result of the trade.
+     *                   *               1 - player2 accepted the trade offer.
+     *                   *               2 - player2 decided to make a counter offer.
+     *                   *               3 - player2 declined the trade offer.
+     *                   *               4 - player2's input was not valid and needs to try again.
+     * @param flavorText the text describing what is happening.
+     */
+    @Override
+    public void showResultOfTradeOffer(int option, String flavorText) {
+
+    }
+}
