f991d1175c36610b2b7e7b9c811579cc22ab2240
Joshua Lenander <42joshl@gmail.com>
2022-12-06 02:56:04 -0500

    Add Game object for construction of Game Entities. Add Main method to run a sample game.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import game.Game;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Game game = new Game();
+        game.startGame();
+    }
+}
