d0647bb150ee2b09dd84455bd89edf005eb21b01
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-12-07 01:08:18 -0500

    reset code

--- src/test/java/turn_use_cases/mortgage_use_case/MortgagePropertyPresenter.java
+++ src/test/java/turn_use_cases/mortgage_use_case/MortgagePropertyPresenter.java
@@ -4,6 +4,8 @@ import game_entities.Player;
 import game_entities.tiles.ColorPropertyTile;
 import game_entities.tiles.Property;
 
+import java.util.ArrayList;
+
 public class MortgagePropertyPresenter implements MortgagePropertyOutputBoundary{
     /**
      * Presents the property which is mortgaged or unmortgaged by player.
@@ -12,17 +14,31 @@ public class MortgagePropertyPresenter implements MortgagePropertyOutputBoundary
      * @param property The property which is mortgaged or unmortgaged by player.
      * @param flavorText the text describing what is happening.
      */
-    @Override
     public void showMortgageProperty(Player player, Property property, String flavorText){}
 
     /**
-     * Presents the property which is mortgaged or unmortgaged by player.
+     * Presents the property which is unmortgaged by player.
      *
-     * @param player the player who wants to mortgage or unmortgage property.
-     * @param property The property which is mortgaged or unmortgaged by player.
+     * @param player the player who wants to unmortgage property.
+     * @param property The property which is unmortgaged by player.
      * @param flavorText the text describing what is happening.
      */
-    @Override
-    public void showMortgageProperty(Player player, ColorPropertyTile property, String flavorText){}
+    public void showUnmortgageProperty(Player player, Property property, String flavorText){}
+
+    /**
+     * Presents the properties which can be mortgaged or unmortgaged.
+     *
+     * @param properties a list of properties which can be mortgaged or unmortgaged.
+     * @param flavorText the text describing what is happening.
+     */
+    public void showMortgagePropertyList(ArrayList<Property> properties, String flavorText){}
+
+    /**
+     * Presents the properties which can be unmortgaged.
+     *
+     * @param properties a list of properties which can be unmortgaged.
+     * @param flavorText the text describing what is happening.
+     */
+    public void showUnmortgagePropertyList(ArrayList<Property> properties, String flavorText){}
 
 }
