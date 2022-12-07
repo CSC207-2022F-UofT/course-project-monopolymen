e095f815a0b49e6e112b8a89cd4147f36a805854
TahirLeonenko <tahir.leonenko@gmail.com>
2022-12-07 16:39:32 -0500

    Made it so the Property Tiles action method only adds money to the owner if the rentPayer has enough money to pay the rent. Changed the action method so that it calls the overloaded subtractMoney with getOwner as player so that if the rentPayer doesn't have enough money it can properly create the LiquiditySituation

--- src/main/java/game_entities/tiles/RailroadTile.java
+++ src/main/java/game_entities/tiles/RailroadTile.java
@@ -86,9 +86,11 @@ public class RailroadTile extends Property{
             return new TileActionResultModel("Would you Like to Purchase " + getTileDisplayName() + " for" + getPurchasePrice() + " ?" , player, player.getPosition());
         }
         else{
-            player.subtractMoney(getRent(player, board.getPropertyTiles()));
-            getOwner().addMoney(getRent(player, board.getPropertyTiles()));
-            return new TileActionResultModel("You Paid" + getRent(player, board.getPropertyTiles()) + " to" + getOwner().getName(), player, player.getPosition());
+            if(getRent(player, board.getPropertyTiles()) <= player.getMoney()){
+                getOwner().addMoney(getRent(player, board.getPropertyTiles()));
+            }
+            player.subtractMoney(getRent(player, board.getPropertyTiles()), getOwner());
+            return new TileActionResultModel("You Paid" + getRent(player, board.getPropertyTiles()) + " to" + getOwner(), player, player.getPosition());
         }
     }
 
