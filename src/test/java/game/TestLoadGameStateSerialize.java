99989aeb824ba5c6044e8b5f55500c887a8dc929
Joshua Lenander <42joshl@gmail.com>
2022-12-07 01:14:55 -0500

    Fix loadGameStateSerialize test with new TurnController transient attribute

--- src/test/java/game/TestLoadGameStateSerialize.java
+++ src/test/java/game/TestLoadGameStateSerialize.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import turn_interface_adapters.TurnController;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -64,7 +65,7 @@ public class TestLoadGameStateSerialize {
         String SAVE_NAME = "loadableGameState";
         save.save(gameState, SAVE_NAME);
 
-        GameState loadedGameState = load.load(SAVE_NAME, save, new TestableGameStatePresenter());
+        GameState loadedGameState = load.load(SAVE_NAME, save, new TestableGameStatePresenter(), new TurnController());
 
         assertEquals(loadedGameState, gameState);
     }
